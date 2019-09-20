24720ba200aadd3aa9a09a7ee01036ed1e2ae939
yc840 <54966502+yc840@users.noreply.github.com>
2019-09-20 00:12:08 -0400

    CREATING WALLS + CAMERA

---
+++ Assets/SCRIPT/CameraControler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour
+{
+    public GameObject PLAYER;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - PLAYER.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = PLAYER.transform.position + offset;
+    }
+}
