f55d5abc3718f08301b3634cf40ff2a2b6543b22
yc840 <54966502+yc840@users.noreply.github.com>
2019-09-20 01:41:55 -0400

    Collison+ UI +Prefabs

--- Assets/SCRIPT/Playercontrol.cs
+++ Assets/SCRIPT/Playercontrol.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Playercontrol : MonoBehaviour{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
     private Rigidbody rb;
+    private int count;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();   
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
     void FixedUpdate()
     {
@@ -20,6 +29,23 @@ public class Playercontrol : MonoBehaviour{
         rb.AddForce(movement);
         
     }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PICK UP"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        } 
 
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "YOU WIN!";
+        }
+    }
 
 }
